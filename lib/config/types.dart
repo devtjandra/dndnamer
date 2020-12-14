a4e2be5e249670fa49588a94577be8e506e728e5
Devina <devtjandra@gmail.com>
2020-12-14 18:29:59 +0800

    Calling API, setting name types

---
+++ lib/config/types.dart
@@ -0,0 +1,16 @@
+const races = [
+  "Human Male",
+  "Human Female",
+  "Dwarvish Male",
+  "Dwarvish Female",
+  "Elvish Male",
+  "Elvish Female",
+  "Halfling Male",
+  "Halfling Female",
+  "Draconic Male",
+  "Draconic Female",
+  "Drow Male",
+  "Drow Female",
+  "Orcish Male",
+  "Orcish Female",
+];
