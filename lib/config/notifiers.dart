35287e279e8377ee18358d0d43db980fdc988df9
Devina <devtjandra@gmail.com>
2020-12-16 01:14:30 +0800

    Getting the providers settled in with the views

--- lib/config/notifiers.dart
+++ lib/config/notifiers.dart
@@ -1,7 +1,14 @@
+import 'package:dndnamer/screens/name_generator/name_generator.dart';
+import 'package:dndnamer/services/api_client.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/all.dart';
 
 class NameGeneratorListNotifier extends StateNotifier<List<String>> {
-  NameGeneratorListNotifier(): super(List());
+  final ProviderReference ref;
+
+  NameGeneratorListNotifier(this.ref) : super(List()) {
+    Future.delayed(Duration.zero, () => getItems());
+  }
 
   void clear() {
     state = List();
@@ -9,5 +16,17 @@ class NameGeneratorListNotifier extends StateNotifier<List<String>> {
 
   void addItems(List<String> items) {
     state.addAll(items);
+  }
+
+  void getItems() async {
+    ref.read(isWaiting).state = true;
+    final raceQuery = ref.watch(race).state;
+    ApiClient().getNames(raceQuery).then((value) {
+      ref.read(isWaiting).state = false;
+      state = value;
+    }).catchError((error) {
+      ref.read(isWaiting).state = false;
+      debugPrint("Error $error");
+    });
   }
 }
--- lib/screens/name_generator/name_generator.dart
+++ lib/screens/name_generator/name_generator.dart
@@ -1,9 +1,31 @@
+import 'package:dndnamer/config/notifiers.dart';
+import 'package:dndnamer/config/types.dart';
+import 'package:dndnamer/services/api_client.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/all.dart';
+
+final isWaiting = StateProvider<bool>((ref) => false);
+final race = StateProvider<String>((ref) => races[0]);
+
+final _nameGeneratorList = StateNotifierProvider<NameGeneratorListNotifier>(
+    (ref) => NameGeneratorListNotifier(ref));
+final _isEmpty =
+    Provider<bool>((ref) => ref.watch(_nameGeneratorList.state).isEmpty);
 
 class NameGenerator extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ScopedReader watch) {
+    return Scaffold(
+        body: Container(
+      child: Column(
+        children: items(watch),
+      ),
+    ));
+  }
 
+  List<Widget> items(ScopedReader watch) {
+    final items = watch(_nameGeneratorList.state);
+    return items.map((e) => Text(e)).toList();
   }
 }
